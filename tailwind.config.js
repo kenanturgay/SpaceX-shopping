a53d317639df11067864f365dab58114a1a0e574
Kenan Turgay <knnturgay@gmail.com>
2025-02-03 14:32:22 +0300

    #T1 I fixed it

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,37 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#FF7B47",
+        secondary: "#0D5C63",
+        fgray: "#737373",
+        lightGray: "#FAFAFA",
+      },
       fontFamily: {
-        montserrat: ["Montserrat", "sans-serif"], // Montserrat fontunu ekledik
+        montserrat: ["Montserrat", "sans-serif"], // Montserrat fontu
+      },
+      fontSize: {
+        h1: "3rem",
+        h2: "2.5rem",
+        h3: "2rem",
+        h4: "1.5rem",
+        h5: "1.25rem",
+        h6: "1.125rem",
+      },
+      fontWeight: {
+        h1: "700",
+        h2: "600",
+        h3: "500",
+        h5: "700",
+        h6: "700",
+      },
+      lineHeight: {
+        h1: "1.2",
+        h2: "1.3",
+        h3: "1.4",
+        h5: "24px",
+        h6: "20px",
       },
     },
   },
